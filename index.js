fe0469f5a362b06544d7cff47834d5fbdbd7e2ce
Brot <gregorsimpson20@gmail.com>
2024-01-05 11:32:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ let app = undefined;
 let data = {
   status: 'waiting',
   result: null,
-  input: {
+  input: null
+  /*input: {
     actions: null,
-  }
+  }*/
 }
 
 function handleError(err) {
@@ -24,30 +25,31 @@ function handleError(err) {
 async function applyActions() {
   data.results = "Working...";
   try {
-    await grist.docApi.applyUserActions(data.input.actions);
-    data.message = 'Done';
+    //await grist.docApi.applyUserActions(data.input.actions);
+    await grist.docApi.applyUserActions(data.input);
+    data.status = 'Done';
   } catch (e) {
-    data.message = `Please grant full access for writing. (${e})`;
+    data.status = `Please grant full access for writing. (${e})`;
   }
 }
 
 function onRecord(row, mappings) {
   try {
     data.status = '';
-    data.results = null;
+    data.result = null;
     // If there is no mapping, test the original record.
     row = grist.mapColumnNames(row) || row;
     if (!row.hasOwnProperty(column)) {
       throw new Error(`Need a visible column named "${column}". You can map a custom column in the Creator Panel.`);
     }
-    const keys = ['actions'];
+    /*const keys = ['actions'];
     if (!row[column] || keys.some(k => !row[column][k])) {
       const allKeys = keys.map(k => JSON.stringify(k)).join(", ");
       const missing = keys.filter(k => !row[column]?.[k]).map(k => JSON.stringify(k)).join(", ");
       const gristName = mappings?.[column] || column;
       throw new Error(`"${gristName}" cells should contain an object with keys ${allKeys}. ` +
         `Missing keys: ${missing}`);
-    }
+    }*/
     data.input = row[column];
   } catch (err) {
     handleError(err);
